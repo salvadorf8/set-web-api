8a591617bf702bd7daf485939b2312c3fdc3ccee
Salvador Flores <salvador.f.8@outlook.com>
2018-11-07 22:29:45 -0600

    SF - added cors

--- index.js
+++ index.js
@@ -2,19 +2,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
+const cors = require('cors');
 require('dotenv').config();
 
 
 const app = express();
 
-
+app.use(cors());
 app.use(bodyParser.json());
 app.unsubscribe(bodyParser.urlencoded({ extended: false }));
 
+app.get('/email/cors', (req, res, next) => {
+    res.json({msg: 'This is CORS-enabled for all origins!'});
+});
 
-app.post('/email/sendform', (req, res) => {
-    console.log(req.body);
-        const htmlEmail = ` 
+app.post('/email/sendform', (req, res, next) => {
+    
+    const htmlEmail = ` 
         <h3>Construction Contact Request</h3> 
         <ul>
             <li>Name: ${req.body.firstName} ${req.body.lastName}</li>
@@ -40,39 +44,39 @@ app.post('/email/sendform', (req, res) => {
         </p>
         `
 
-        let transporter = nodemailer.createTransport({
-            host: process.env.HOST,
-            port: 587,
-            secure: false, // true for 465, false for other ports
-            auth: {
-                user: process.env.USER, // generated ethereal user
-                pass: process.env.PASSWORD // generated ethereal password
-            }
-        });
-    
-        let mailOptions = {
-            from: process.env.USER, // sender address
-            to: process.env.USER, // list of receivers
-            subject: 'Construction inquiry', // Subject line
-            text: req.body.message, // plain text body
-            html: htmlEmail // html body
+    let transporter = nodemailer.createTransport({
+        host: process.env.HOST,
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: process.env.USER, // generated ethereal user
+            pass: process.env.PASSWORD // generated ethereal password
         }
+    });
 
-        transporter.sendMail(mailOptions, (error, info) => {
-            if (error) {
-                console.log(error);
-                res.json(error);
-            } else {
-                console.log('Envelope: %s', info.envelope);
-                console.log('MessageId: %s', info.messageId);
-                res.status(200).json({"msg": "Message has been sent"});
-            }
-            return res.end();
-        });
+    let mailOptions = {
+        from: process.env.USER, // sender address
+        to: process.env.USER, // list of receivers
+        subject: 'Construction inquiry', // Subject line
+        text: req.body.message, // plain text body
+        html: htmlEmail // html body
+    }
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.log(error);
+            res.json(error);
+        } else {
+            console.log('Envelope: %s', info.envelope);
+            console.log('MessageId: %s', info.messageId);
+            res.status(200).json({ "msg": "Message has been sent" });
+        }
+        return res.end();
+    });
 });
 
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
-    console.log(`Server listening on port ${ PORT }`);
+    console.log(`Server listening on port ${PORT}`);
 });
